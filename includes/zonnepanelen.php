9be5f233336625c7e3ac5cf74ba69c55f86bf59d
fieryhavoc <@>
2026-06-19 09:38:48 +0200

    zonnepanelen grafiek v1

--- includes/zonnepanelen.php
+++ includes/zonnepanelen.php
@@ -1,2 +1,4 @@
-<h2>Zonnepanelen</h2>
-<div class="zonnepanelen-info"></div>
+<h2>Zonnepaneel informatie</h2>
+<div class="zonnepanelen-info" style="text-align:center;">
+    <img src="Zonnepanelen.png" alt="Zonnepanelen" style="max-width:100%; height:auto; display:inline-block;" />
+</div>
